033b29d7e752b5970f3b78d543beb3060c08c435
Harry.G <40484678+N-Pu@users.noreply.github.com>
2021-06-04 20:38:59 +0300

    Update matrix_m_Cannon.h

--- modules/task_3/grigoryan_g_matrix_Cannon_TBB/matrix_m_Cannon.h
+++ modules/task_3/grigoryan_g_matrix_Cannon_TBB/matrix_m_Cannon.h
@@ -8,10 +8,13 @@
 #include <cmath>
 
 using matrix = std::vector<std::vector<double>>;
+
 matrix RandomMatrix(const int n);
+
 matrix NaiveMulti(const matrix &A, const matrix &B);
 matrix BlockMulti(const matrix &A, const matrix &B, const int &blockSize);
 matrix AlgorithmCannonTBB(const matrix &A, const matrix &B);
+
 bool CompareMatrix(const matrix &A, const matrix &B);
 bool CompareValues(const double &a, const double &b);
 
