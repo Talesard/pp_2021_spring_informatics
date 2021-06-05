c29cfac4cdd79582f682f4cdcc1df86947b078f4
Andrey0948 <andrey0948@mail.ru>
2021-06-05 20:12:22 +0300

    lab

--- modules/task_1/gapon_a_algorithm_dijkstra/dijkstra.h
+++ modules/task_1/gapon_a_algorithm_dijkstra/dijkstra.h
@@ -3,29 +3,8 @@
 #define MODULES_TASK_1_GAPON_A_ALGORITHM_DIJKSTRA_DIJKSTRA_H_
 
 #include <vector>
-#include <utility>
-#include <iostream>
-
-class Graph {
- public:
-    explicit Graph(const std::vector<std::vector<size_t>>& distanceMatrix);
-    explicit Graph(const size_t vertexCount = 10, const size_t edgeCount = 45);
-    ~Graph() = default;
-
-    std::vector<std::vector<size_t>> getDistanceMatrix() const;
-    size_t getVertexCount() const;
-
-    friend std::ostream& operator<<(std::ostream& out, const Graph& graph);
- private:
-    void generate();
-
-    size_t _vertexCount;
-    size_t _edgeCount;
-    std::vector<std::vector<size_t>> _distanceMatrix;
-};
-
-std::vector<size_t> algorithmDijkstra(const Graph& graph, const size_t root);
-size_t getVertexWithMinDistance(const std::vector<std::pair<size_t, bool>>& vertices);
-void printDistances(const std::vector<size_t>& distances, const size_t root);
+std::vector<int> dijkstra_algorithm(const std::vector<int>& graph,
+int first, int end);
+std::vector<int> Create_Graph(int size);
 
 #endif  // MODULES_TASK_1_GAPON_A_ALGORITHM_DIJKSTRA_DIJKSTRA_H_
